82acd80d0dea4e792f83f18682f6dfa4ae5b3860
tgoelles <thomas.goelles@gmail.com>
2023-01-08 01:11:07 +0100

    added automatic update from CV data

--- update_publications.sh
+++ update_publications.sh
@@ -3,11 +3,13 @@
 # This script updates the publications page of my website from my CV
 
 # download bib file from github repo
-curl -s https://github.com/tgoelles/CV/blob/master/presentations.bib > presentations.bib
-curl -s https://github.com/tgoelles/CV/blob/master/publications.bib > publications.bib
+curl https://raw.githubusercontent.com/tgoelles/CV/master/publications.bib\?token=GHSAT0AAAAAAB4UPPXINVV4W7AHF4P5LPBOY52C5KQ > presentations.bib
+curl https://raw.githubusercontent.com/tgoelles/CV/master/presentations.bib\?token=GHSAT0AAAAAAB4UPPXIDWUXZQFLIBAGOIJ6Y52C6AA > publications.bib
 
 # concat bib files
 cat presentations.bib publications.bib > all_publications.bib
 
 # run academic CLI to update publications page
-academic import --bibtex all_publications.bib
+academic import --bibtex all_publications.bib --verbose
+
+https://raw.githubusercontent.com/username/repository/branch

1c32a1e72af4a1121af95b99a70a1d6334024fbb
tgoelles <thomas.goelles@gmail.com>
2023-01-08 01:24:47 +0100

    better pub

--- update_publications.sh
+++ update_publications.sh
@@ -3,8 +3,8 @@
 # This script updates the publications page of my website from my CV
 
 # download bib file from github repo
-curl https://raw.githubusercontent.com/tgoelles/CV/master/publications.bib\?token=GHSAT0AAAAAAB4UPPXINVV4W7AHF4P5LPBOY52C5KQ > presentations.bib
-curl https://raw.githubusercontent.com/tgoelles/CV/master/presentations.bib\?token=GHSAT0AAAAAAB4UPPXIDWUXZQFLIBAGOIJ6Y52C6AA > publications.bib
+curl https://raw.githubusercontent.com/tgoelles/CV/master/publications.bib\?token=GHSAT0AAAAAAB4UPPXINVV4W7AHF4P5LPBOY52C5KQ > publications.bib
+curl https://raw.githubusercontent.com/tgoelles/CV/master/presentations.bib\?token=GHSAT0AAAAAAB4UPPXIDWUXZQFLIBAGOIJ6Y52C6AA > presentations.bib
 
 # concat bib files
 cat presentations.bib publications.bib > all_publications.bib
@@ -12,4 +12,5 @@ cat presentations.bib publications.bib > all_publications.bib
 # run academic CLI to update publications page
 academic import --bibtex all_publications.bib --verbose
 
-https://raw.githubusercontent.com/username/repository/branch
+# replace Thomas Goelles with admin in all markdown files in the content/publication folder
+find content/publication -type f -exec sed -i '' 's/Thomas Goelles/admin/g' {} +

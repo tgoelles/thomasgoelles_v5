48ef8028cd109fa370585e2353500c7607206b27
tgoelles <thomas.goelles@gmail.com>
2023-08-30 07:51:24 +0200

    better automatic pub

--- update_publications.sh
+++ update_publications.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# activate conda web ennvironment
+# conda activate web
+
 # This script updates the publications page of my website from my CV
 
 # download bib file from github repo
@@ -11,11 +14,11 @@
 cat presentations.bib publications.bib > all_publications.bib
 
 # clean up bib file
-bibtex-tidy --months --tab --align=13 --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --generate-keys="[auth:required:lower][year:required][duplicateNumber]" all_publications.bib
-
+sed -E '/^[[:space:]]*month[[:space:]]*=/s/[0-9-]//g' all_publications.bib > all_publications_clean.bib
+bibtex-tidy --months --tab --align=13 --duplicates=doi --merge=combine --no-escape --sort-fields --no-remove-dupe-fields --generate-keys="[auth:required:lower][year:required][veryshorttitle:lower][duplicateNumber]"  all_publications_clean.bib
 
 # run academic CLI to update publications page
-academic import --bibtex all_publications.bib --verbose
+academic import --bibtex all_publications_clean.bib --verbose
 
 # replace Thomas Goelles with admin in all markdown files in the content/publication folder
 find content/publication -name "*.md" -exec sed -i '' 's/Thomas Goelles/admin/g' {} +
